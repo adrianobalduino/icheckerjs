8fb7e84879347cc8d985dff2801fb62d8029190b
Adriano Balduino <adrianobalduino90@gmail.com>
2022-09-24 11:55:44 -0300

    Selection and Dimension Mode

--- app.js
+++ app.js
@@ -3,11 +3,44 @@ import { IfcViewerAPI } from "web-ifc-viewer";
 
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({container, backgroundColor: new Color(0xffffff)});
-
-viewer.axes.setAxes();
 viewer.grid.setGrid();
+viewer.axes.setAxes();
 
 const input = document.getElementById("file-input");
+const selection = document.getElementById("btn-selection");
+const dimension = document.getElementById("btn-dimension");
+
+selection.addEventListener(
+    "click",
+    async (clicked) => {
+        viewer.dimensions.active= false;
+        viewer.dimensions.previewActive = false;
+        dimension.className="btn btn-light";
+        selection.className="btn btn-dark";
+        window.ondblclick = () => viewer.IFC.selector.pickIfcItem();
+        window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+    },
+    )
+
+dimension.addEventListener(
+    "click", 
+    async (clicked) => {
+        selection.className="btn btn-light";
+        dimension.className="btn btn-dark";
+        viewer.dimensions.active= true;
+        viewer.dimensions.previewActive = true;
+
+        window.ondblclick = () => {
+            viewer.dimensions.create();
+        }
+
+        window.onkeydown = (event) => {
+            if(event.code === 'Delete'){
+                viewer.dimensions.delete();
+            }
+        }
+}, false
+ );
 
 input.addEventListener(
     "change",
@@ -15,8 +48,11 @@ input.addEventListener(
         const ifcURL = URL.createObjectURL(changed.target.files[0]);
         const model = await viewer.IFC.loadIfcUrl(ifcURL);
         await viewer.shadowDropper.renderShadow(model.modelID);
+        window.ondblclick = () => viewer.IFC.selector.pickIfcItem();
+        window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
         },
     false
 );
 
+    
 
--- bundle.js
+++ bundle.js
@@ -44168,12 +44168,12 @@ class IfcComponent {
     }
     update(_delta) { }
 }
-var dimension;
+var dimension$1;
 (function (dimension) {
     dimension["x"] = "x";
     dimension["y"] = "y";
     dimension["z"] = "z";
-})(dimension || (dimension = {}));
+})(dimension$1 || (dimension$1 = {}));
 
 const _raycaster$1 = new Raycaster();
 
@@ -121783,11 +121783,44 @@ class IfcViewerAPI {
 
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({container, backgroundColor: new Color(0xffffff)});
-
-viewer.axes.setAxes();
 viewer.grid.setGrid();
+viewer.axes.setAxes();
 
 const input = document.getElementById("file-input");
+const selection = document.getElementById("btn-selection");
+const dimension = document.getElementById("btn-dimension");
+
+selection.addEventListener(
+    "click",
+    async (clicked) => {
+        viewer.dimensions.active= false;
+        viewer.dimensions.previewActive = false;
+        dimension.className="btn btn-light";
+        selection.className="btn btn-dark";
+        window.ondblclick = () => viewer.IFC.selector.pickIfcItem();
+        window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+    },
+    );
+
+dimension.addEventListener(
+    "click", 
+    async (clicked) => {
+        selection.className="btn btn-light";
+        dimension.className="btn btn-dark";
+        viewer.dimensions.active= true;
+        viewer.dimensions.previewActive = true;
+
+        window.ondblclick = () => {
+            viewer.dimensions.create();
+        };
+
+        window.onkeydown = (event) => {
+            if(event.code === 'Delete'){
+                viewer.dimensions.delete();
+            }
+        };
+}, false
+ );
 
 input.addEventListener(
     "change",
@@ -121795,6 +121828,8 @@ input.addEventListener(
         const ifcURL = URL.createObjectURL(changed.target.files[0]);
         const model = await viewer.IFC.loadIfcUrl(ifcURL);
         await viewer.shadowDropper.renderShadow(model.modelID);
+        window.ondblclick = () => viewer.IFC.selector.pickIfcItem();
+        window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
         },
     false
 );
